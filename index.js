63ba336199e50c799472d4ef611b04f182e4523f
IlanTauscher <48240500@est.ort.edu.ar>
2025-06-27 10:15:39 -0300

    Add Supabase configuration guide

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://snskgnsnvnpsofbuglfo.supabase.co'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
 
     
